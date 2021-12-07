7782d627f72e64d40e5ac02aa4c005d3c21169cb
seho <1018715564@qq.com>
2021-12-07 21:59:33 +0800

    ✨ feat: 完成猜数字的游戏

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,10 +1,32 @@
 use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io::{stdin, stdout, BufWriter};
 
 fn main() {
     let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
+    let message = String::from("游戏结束🎮");
     let width = message.chars().count();
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+    // 生成一个1-100随机值
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("秘密数字已生成, 请在1-100中猜一个数字，输入非法字符会自动结束");
+    loop {
+        let mut guess = String::new();
+        stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("你猜的数字是: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("你猜对了");
+                break;
+            },
+            Ordering::Greater => println!("你猜的有点大oh～，再试试吧"),
+            Ordering::Less => println!("你猜的有点小oh～，再试试吧"),
+        }
+    }
 }
