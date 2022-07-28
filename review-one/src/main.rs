d0ffde58b649229aeb2e25ce7848e27ac3cc1ca1
seho <1018715564@qq.com>
2022-07-28 23:39:24 +0800

    ✨ feat: 变量结构和mut

--- review-one/src/main.rs
+++ review-one/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
-    let mut a = "hhh";
-    a = "seho";
-    println!("Hello, world! {}", a);
+    let a = "hhh";
+    println!("Hello, world! {} && {}", add(1, 2), a);
+    // 测试一下没有使用的变量, 用_解决
+    let _b = "";
+    // 变量结构
+    let (c, d) = (true, false);
+    println!("{} {}", c, d);
 }
 
-
- // 定义一个函数，输入两个i32类型的32位有符号整数，返回它们的和
- fn add(i: i32, j: i32) -> i32 {
+// 定义一个函数，输入两个i32类型的32位有符号整数，返回它们的和
+fn add(i: i32, j: i32) -> i32 {
     // 返回相加值，这里可以省略return
     i + j
-  }
+}
