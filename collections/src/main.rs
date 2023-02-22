7345ecb5ad086677b08840d7ec6e49852434a5e5
seho <seho.zaq@gmail.com>
2023-02-22 22:51:31 +0800

    feat: add string & hashmap

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, world!");
     let mut v = vec![1, 2, 3];
@@ -16,4 +18,24 @@ fn main() {
     for i in &v {
         println!("{i}");
     }
+    // string字符串其实就是字节的集合
+    let mut s = String::new();
+    let data = "initial contents";
+    let mut s = data.to_string();
+    // 拼接字符串
+    s.push_str("foo");
+    s.push('l');
+    // 也可以使用 + 来拼接字符串
+    let mut s = s + "bar";
+    println!("{}", s);
+    // + 操作其实是一个add函数
+    // fn add(self, s: &str) -> String {
+    // 第一个参数其实是借用，所以s之后就会被释放，第二个参数是一个引用，所以不会被释放
+    // 如果我要拼接多个字符串，可以优雅的使用format宏
+    let  s = format!("{s}-{data}");
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 }
