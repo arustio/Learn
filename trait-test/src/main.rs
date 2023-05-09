62ab175e6627223260db33652eff40bb1065c635
seho <seho.zaq@gmail.com>
2023-05-09 23:54:46 +0800

    feat: add trait object

--- trait-test/src/main.rs
+++ trait-test/src/main.rs
@@ -66,6 +66,44 @@ fn return_summarizable() -> impl Summary {
     Post{title: "Rust语言简介".to_string(),author: "Sunface".to_string(), content: "Rust棒极了!".to_string()}
 }
 
+// 一个函数如何返回多个特征类型，就要使用特征对象概念
+trait Draw {
+    fn draw(&self) -> String;
+}
+
+impl Draw for u8 {
+    fn draw(&self) -> String {
+        format!("u8: {}", *self)
+    }
+}
+
+impl Draw for f64 {
+    fn draw(&self) -> String {
+        format!("f64: {}", *self)
+    }
+}
+
+// 若 T 实现了 Draw 特征， 则调用该函数时传入的 Box<T> 可以被隐式转换成函数参数签名中的 Box<dyn Draw>
+fn draw1(x: Box<dyn Draw>) {
+    // 由于实现了 Deref 特征，Box 智能指针会自动解引用为它所包裹的值，然后调用该值对应的类型上定义的 `draw` 方法
+    x.draw();
+}
+
+fn draw2(x: &dyn Draw) {
+    x.draw();
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
 
 fn main() {
     let post = Post{title: "Rust语言简介".to_string(),author: "Sunface".to_string(), content: "Rust棒极了!".to_string()};
@@ -73,4 +111,6 @@ fn main() {
 
     println!("{}",post.summarize());
     println!("{}",weibo.summarize());
+
+    
 }
