a1eaf88977017389f774bb963aec00f5c044288a
seho <seho.zaq@gmail.com>
2023-06-04 21:40:16 +0800

    feat: update module demo

--- module-test-2/src/lib.rs
+++ module-test-2/src/lib.rs
@@ -13,37 +13,7 @@ mod tests {
     }
 }
 
-pub mod front_of_house {
-    pub mod hosting {
-
-        // 给枚举设置pub时，其成员会自动设置为pub
-        pub enum Appetizer {
-            Soup,
-            Salad,
-        }
-
-        // 给结构体设置pub时，其字段不会自动设置为pub
-        pub struct Breakfast {
-            pub toast: String,
-            seasonal_fruit: String,
-        }
-
-        pub fn add_to_waitlist() {
-            print!("add_to_waitlist");
-            super::hosting::seat_at_table();
-        }
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 // 使用一个别名来引用路径, 也可以加入pub关键字，让其他人使用包时，可以直接使用hosting作用域
 use crate::front_of_house::hosting as HaoHosting;
@@ -58,3 +28,5 @@ pub fn eat_at_restaurant() {
     // 相对路径
     front_of_house::hosting::add_to_waitlist();
 }
+
+use std::collections::{HashMap,BTreeMap,HashSet};
